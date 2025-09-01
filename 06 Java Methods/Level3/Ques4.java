9a5b631e7776353323ef9976342fecae7566a182
Sukanshi Garg <sukanshigargagra123@gmail.com>
2025-09-01 12:06:30 +0530

    Delete 06 Java Methods directory

--- 06 Java Methods/Level3/Ques4.java
+++
@@ -1,9 +0,0 @@
-public class Ques4 extends NumberChecker {
-    public static void main(String[] args) {
-        int num = 12321; // Example number
-
-        System.out.println("Number: " + num);
-        System.out.println("Is Palindrome? " + isPalindrome(num));
-        System.out.println("Is Duck Number? " + isDuck(num));
-    }
-}
