bf18a710d7b3a9936a3f702a5a50df8712e80824
Sukanshi Garg <sukanshigargagra123@gmail.com>
2025-09-01 12:05:45 +0530

    Delete 03 Arrays directory

--- 03 Arrays/Level-1/number.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class number {
-    public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       System.out.println("Enter the number to be checked ");
-       int n=sc.nextInt();
-    }
-}
